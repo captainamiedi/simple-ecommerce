f27fc1523644cc3249915b536f7bf96a62e0fd1d
Bright Amidiagbe <bright@norebase.com>
2024-08-30 00:46:04 +0100

    handle api error

--- Service/index.ts
+++ Service/index.ts
@@ -1,9 +1,13 @@
-export const fetchProducts = async() => {
+export const fetchProducts = async () => {
     try {
-        const response = await fetch('https://fakestoreapi.com/products')
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const data = await response.json();
-        return data
+        return data;
     } catch (error) {
-        
+        console.error('Error fetching products:', error);
+        throw error;
     }
 }
